21286e503c4040326f12902b0e4e747a9bb92267
Born-Bohrer <chem1000leaves@gmail.com>
2025-03-22 13:55:19 +0900

    aaa

--- src/components/GachaResult.tsx
+++ src/components/GachaResult.tsx
@@ -5,9 +5,11 @@ import './GachaResult.css';
 import ExtLinkIcon from "./box-arrow-up-right";
 
 const GachaResult: FC = () => {
-    const init = localStorage.getItem('last_shown_lang_ID');
+    const init = Number(localStorage.getItem('last_shown_lang_ID'));
 
-    const [index, setIndex] = useState(Number(init));
+    const cond = init >= 0 && init < contents.length;
+
+    const [index, setIndex] = useState(cond ? init : 0);
     
     const result_ref = useRef<HTMLDivElement | null>(null);
 
@@ -62,162 +64,160 @@ const GachaResult: FC = () => {
                 ガチャを回す！
             </button>
 
-            {(
-                <div ref={result_ref} className="flex flex-col gap-y-3 relative" id="result-root" data-visible="true">
-                    <h3 id="kekka" className="text-2xl font-medium font-serif text-center">〜結果〜</h3>
+            <div ref={result_ref} className="flex flex-col gap-y-3 relative" id="result-root" data-visible="true">
+                <h3 id="kekka" className="text-2xl font-medium font-serif text-center">〜結果〜</h3>
 
-                    {/* 名前, 作者, 説明, 創作時期, サイト, 辞書, 文法, twitter, 世界, カテゴリ, clav3 */}
+                {/* 名前, 作者, 説明, 創作時期, サイト, 辞書, 文法, twitter, 世界, カテゴリ, clav3 */}
 
-                    <table id="result-table">
-                        <tbody>
+                <table id="result-table">
+                    <tbody>
+                        <tr>
+                            <td>名前</td>
+                            <td>{lang.name.normal.concat(lang.name.kanji ?? []).join(', ')}</td>
+                        </tr>
+                        <tr>
+                            <td>作者</td>
+                            <td>{lang.creator.join(', ')}</td>
+                        </tr>
+                        {lang.desc && (
                             <tr>
-                                <td>名前</td>
-                                <td>{lang.name.normal.concat(lang.name.kanji ?? []).join(', ')}</td>
+                                <td>説明</td>
+                                <td>
+                                    {
+                                        lang.desc.map((par, i) => {
+                                            return <p key={`${index}-${i}`}>{par}</p>;
+                                        })
+                                    }
+                                </td>
                             </tr>
+                        )}
+                        {lang.period && (
                             <tr>
-                                <td>作者</td>
-                                <td>{lang.creator.join(', ')}</td>
+                                <td>創作時期</td>
+                                <td>{lang.period}</td>
                             </tr>
-                            {lang.desc && (
-                                <tr>
-                                    <td>説明</td>
-                                    <td>
+                        )}
+                        {lang.site && (
+                            <tr>
+                                <td>サイト</td>
+                                <td>
+                                    <ul>
                                         {
-                                            lang.desc.map((par, i) => {
-                                                return <p key={`${index}-${i}`}>{par}</p>;
-                                            })
-                                        }
-                                    </td>
-                                </tr>
-                            )}
-                            {lang.period && (
-                                <tr>
-                                    <td>創作時期</td>
-                                    <td>{lang.period}</td>
-                                </tr>
-                            )}
-                            {lang.site && (
-                                <tr>
-                                    <td>サイト</td>
-                                    <td>
-                                        <ul>
-                                            {
-                                                lang.site.map((sit, i) => {
+                                            lang.site.map((sit, i) => {
 
-                                                    if (sit.name) {
-                                                        if (sit.name.includes('サイト') || sit.name.includes('辞書') || sit.name.includes('文法')) {
-                                                            return null;
-
-                                                        } else {
-                                                            return <li key={`${index}-${i}`}>{sit.name}: <a href={sit.url} target="_blank" rel="noreferrer">{sit.url} <ExtLinkIcon /></a></li>;
-                                                        }
-                                                    } else {
-                                                        return <li key={`${index}-${i}`}><a href={sit.url} target="_blank" rel="noreferrer">{sit.url} <ExtLinkIcon /></a></li>;
-                                                    }
-                                                })
-                                            }
-                                        </ul>
-                                    </td>
-                                </tr>
-                            )}
-                            {lang.dict && (
-                                <tr>
-                                    <td>辞書</td>
-                                    <td>
-                                        <ul>
-                                            {
-                                                lang.dict.map((dic, i) => {
-                                                    return (
-                                                        <li key={`${index}-${i}`}>
-                                                            <a href={dic} target="_blank" rel="noreferrer">{dic} <ExtLinkIcon /></a>
-                                                        </li>
-                                                    );
-                                                })
-                                            }
-                                        </ul>
-                                    </td>
-                                </tr>
-                            )}
-                            {lang.grammar && (
-                                <tr>
-                                    <td>文法</td>
-                                    <td>
-                                        <ul>
-                                            {
-                                                lang.grammar.map((gram, i) => {
-                                                    return (
-                                                        <li key={`${index}-${i}`}>
-                                                            <a href={gram} target="_blank" rel="noreferrer">{gram} <ExtLinkIcon /></a>
-                                                        </li>
-                                                    );
-                                                })
-                                            }
-                                        </ul>
-                                    </td>
-                                </tr>
-                            )}
-                            {lang.twitter && (
-                                <tr>
-                                    <td>𝕏 (旧twitter)</td>
-                                    <td>
-                                        <ul>
-                                            {
-                                                lang.twitter.map((url, i) => {
-                                                    return (
-                                                        <li key={`${index}-${i}`}>
-                                                            <a href={url} target="_blank" rel="noreferrer">{url} <ExtLinkIcon /></a>
-                                                        </li>
-                                                    );
-                                                })
-                                            }
-                                        </ul>
-                                    </td>
-                                </tr>
-                            )}
-                            {lang.world && (
-                                <tr>
-                                    <td>世界</td>
-                                    <td>{lang.world.join(', ')}</td>
-                                </tr>
-                            )}
-                            {lang.category && (
-                                <tr>
-                                    <td>カテゴリ</td>
-                                    <td>
-                                        <ul>
-                                            {
-                                                lang.category.map((cat, i) => {
-                                                    if (cat.name === 'CLA v3' || cat.name === 'モユネ分類') {
+                                                if (sit.name) {
+                                                    if (sit.name.includes('サイト') || sit.name.includes('辞書') || sit.name.includes('文法')) {
                                                         return null;
 
-                                                    } else if (cat.content) {
-                                                        return <li key={`${index}-${i}`}>{cat.name}: {cat.content}</li>;
-
                                                     } else {
-                                                        return <li key={`${index}-${i}`}>{cat.name}</li>;
+                                                        return <li key={`${index}-${i}`}>{sit.name}: <a href={sit.url} target="_blank" rel="noreferrer">{sit.url} <ExtLinkIcon /></a></li>;
                                                     }
-                                                })
-                                            }
-                                        </ul>
-                                    </td>
-                                </tr>
-                            )}
-                            {lang.moyune && (
-                                <tr>
-                                    <td>モユネ分類</td>
-                                    <td>{lang.moyune.join('/')}</td>
-                                </tr>
-                            )}
-                            {lang.clav3 && (
-                                <tr>
-                                    <td>CLA v3</td>
-                                    <td>{lang.clav3.dialect}_{lang.clav3.language}_{lang.clav3.family}_{lang.clav3.creator}</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-            )}
+                                                } else {
+                                                    return <li key={`${index}-${i}`}><a href={sit.url} target="_blank" rel="noreferrer">{sit.url} <ExtLinkIcon /></a></li>;
+                                                }
+                                            })
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        )}
+                        {lang.dict && (
+                            <tr>
+                                <td>辞書</td>
+                                <td>
+                                    <ul>
+                                        {
+                                            lang.dict.map((dic, i) => {
+                                                return (
+                                                    <li key={`${index}-${i}`}>
+                                                        <a href={dic} target="_blank" rel="noreferrer">{dic} <ExtLinkIcon /></a>
+                                                    </li>
+                                                );
+                                            })
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        )}
+                        {lang.grammar && (
+                            <tr>
+                                <td>文法</td>
+                                <td>
+                                    <ul>
+                                        {
+                                            lang.grammar.map((gram, i) => {
+                                                return (
+                                                    <li key={`${index}-${i}`}>
+                                                        <a href={gram} target="_blank" rel="noreferrer">{gram} <ExtLinkIcon /></a>
+                                                    </li>
+                                                );
+                                            })
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        )}
+                        {lang.twitter && (
+                            <tr>
+                                <td>𝕏 (旧twitter)</td>
+                                <td>
+                                    <ul>
+                                        {
+                                            lang.twitter.map((url, i) => {
+                                                return (
+                                                    <li key={`${index}-${i}`}>
+                                                        <a href={url} target="_blank" rel="noreferrer">{url} <ExtLinkIcon /></a>
+                                                    </li>
+                                                );
+                                            })
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        )}
+                        {lang.world && (
+                            <tr>
+                                <td>世界</td>
+                                <td>{lang.world.join(', ')}</td>
+                            </tr>
+                        )}
+                        {lang.category && (
+                            <tr>
+                                <td>カテゴリ</td>
+                                <td>
+                                    <ul>
+                                        {
+                                            lang.category.map((cat, i) => {
+                                                if (cat.name === 'CLA v3' || cat.name === 'モユネ分類') {
+                                                    return null;
+
+                                                } else if (cat.content) {
+                                                    return <li key={`${index}-${i}`}>{cat.name}: {cat.content}</li>;
+
+                                                } else {
+                                                    return <li key={`${index}-${i}`}>{cat.name}</li>;
+                                                }
+                                            })
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        )}
+                        {lang.moyune && (
+                            <tr>
+                                <td>モユネ分類</td>
+                                <td>{lang.moyune.join('/')}</td>
+                            </tr>
+                        )}
+                        {lang.clav3 && (
+                            <tr>
+                                <td>CLA v3</td>
+                                <td>{lang.clav3.dialect}_{lang.clav3.language}_{lang.clav3.family}_{lang.clav3.creator}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
             
         </div>
     );
