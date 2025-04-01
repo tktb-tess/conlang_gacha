b9cb84e5f2667299e5ea6ceab029a44013d00621
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-01 16:23:53 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import App from "./App";
+import App from "./Apps";
 
 const root = document.querySelector<HTMLDivElement>('div#root');
 if (!root) throw Error('cannot get `root`');
