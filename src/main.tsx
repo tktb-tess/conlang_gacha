863554b5b165bf739b43e7242c04e4b86bae38e0
Born-Bohrer <chem1000leaves@gmail.com>
2025-04-02 18:58:13 +0900

    darma

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./Apps";
 
-const root = document.querySelector<HTMLDivElement>('div#root');
-if (!root) throw Error('cannot get `root`');
+const root = document.querySelector<HTMLDivElement>("div#root");
+if (!root) throw Error("cannot get `root`");
+
+localStorage.removeItem("last_shown_lang_ID");
+localStorage.removeItem("last-shown-lang-ID");
+localStorage.removeItem("ctc_cache");
 
 ReactDOM.createRoot(root).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
